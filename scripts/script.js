e64cd66b2d382ac02d5bc67c59384e8ebe32941f
Chandan K P <chandankp.xworkz@gmail.com>
2023-09-23 22:30:25 +0530

    ADDED THE COMPLET JS LOGIC
    the game hase 5 rounds and finally one with the highest wins is the winner

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,52 @@
-
-let playerSelection = prompt('enter choice');
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-  const choices = [ 'ROCK', 'PAPER','SCISSSORS'];
   return choices[Math.floor(Math.random() * 3)];
 }
+
+function getPlayerChoice() {
+  const choice = prompt(
+    `Enter your choice:
+          1. ROCK
+          2. PAPER
+          3. SCISSORS`
+  );
+  return choices[Number(choice - 1)];
+}
+
+function playRound(player, computer) {
+  player = player.toLowerCase();
+  if (player === computer) {
+    return "tie";
+  } else if (
+    (player === choices[0] && computer === choices[2]) ||
+    (player === choices[1] && computer === choices[0]) ||
+    (player === choices[2] && computer === choices[1])
+  ) {
+    return "player";
+  } else {
+    return "computer";
+  }
+}
+
+let game = () => {
+  let pwin = 0,
+    cwin = 0;
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+
+    let whoWon = playRound(playerSelection, computerSelection);
+
+    if (whoWon === "player") {
+      pwin += 1;
+    } else if (whoWon === "computer") {
+      cwin += 1;
+    }
+  }
+  if (pwin === cwin) console.log("tie");
+  else if (pwin > cwin) console.log("player wins");
+  else if (cwin > pwin) console.log("computer wins");
+};
+
+game();
