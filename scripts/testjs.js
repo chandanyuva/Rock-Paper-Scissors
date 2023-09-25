2513a5337456810a9845ad84dc836502732a85ac
Chandan K P <chandankp.xworkz@gmail.com>
2023-09-26 03:29:23 +0530

    CREATED A DIV TO DISPLAY CURRENT ROUND WINNER
    using a div and p tag to display current winner
    TODO: need to add a counter to keep track of score

--- scripts/testjs.js
+++ scripts/testjs.js
@@ -1,3 +1,7 @@
+const resultDiv= document.createElement('div');
+const rp=document.createElement('p');
+resultDiv.appendChild(rp);
+
 const choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
@@ -37,16 +41,24 @@ btnscissors.addEventListener('click',(event)=>{
 // one round play
 
 function playRound(player, computer) {
-    console.log(`${player} ${computer}`);
+    // console.log(`${player} ${computer}`);
     if (player === computer) {
-      console.log("tie");
+        rp.textContent=`tie`;
+        console.log("tie");
     } else if (
       (player === choices[0] && computer === choices[2]) ||
       (player === choices[1] && computer === choices[0]) ||
       (player === choices[2] && computer === choices[1])
     ) {
+        rp.textContent=`player`;
       console.log("player");
     } else {
+        rp.textContent=`computer`;
         console.log("computer");
     }
   }
+
+ document.body.appendChild(resultDiv);
+
+
+  
