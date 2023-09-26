f3fed6c158b10ca776fa9c3097394d9b9ad1f797
Chandan K P <chandankp.xworkz@gmail.com>
2023-09-26 15:22:15 +0530

    FINALLY MADE THE UI
    added css and completed the js logic for a 5 round match
    TODO: need to add the results to the page dynamically

--- scripts/testjs.js
+++ scripts/testjs.js
@@ -1,64 +1,92 @@
-const resultDiv= document.createElement('div');
-const rp=document.createElement('p');
-resultDiv.appendChild(rp);
 
 const choices = ["rock", "paper", "scissors"];
+let pwin = 0, cwin = 0;
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
-  }
-
-function btnChoice(btnID){
-switch (btnID){
-    case 'btn-rock':
-    return 1;
-    case 'btn-paper':
-    return 2;
-    case 'btn-scissors':
-    return 3;
 }
+
+function btnChoice(btnID) {
+    switch (btnID) {
+        case "btn-rock":
+            return 1;
+        case "btn-paper":
+            return 2;
+        case "btn-scissors":
+            return 3;
+    }
 }
 
 function getPlayerChoice(btnchoice) {
-const choice = btnchoice;
-return choices[Number(choice - 1)];
+    const choice = btnchoice;
+    return choices[Number(choice - 1)];
 }
 
-const btnRock = document.getElementById('btn-rock');
-btnRock.addEventListener('click',(event)=>{
-    playRound(getPlayerChoice(btnChoice(event.target.id)),getComputerChoice());
-})
-const btnPaper = document.getElementById('btn-paper');
-btnPaper.addEventListener('click',(event)=>{
-    playRound(getPlayerChoice(btnChoice(event.target.id)),getComputerChoice());
-})
-const btnscissors = document.getElementById('btn-scissors');
-btnscissors.addEventListener('click',(event)=>{
-    playRound(getPlayerChoice(btnChoice(event.target.id)),getComputerChoice());
-})
-
-
+const btnRock = document.getElementById("btn-rock");
+btnRock.addEventListener("click", (event) => {
+    playmatch(event);
+});
+const btnPaper = document.getElementById("btn-paper");
+btnPaper.addEventListener("click", (event) => {
+    playmatch(event);
+});
+const btnscissors = document.getElementById("btn-scissors");
+btnscissors.addEventListener("click", (event) => {
+    playmatch(event);
+});
 
 // one round play
-
+const rrs=document.getElementById('round-result')
 function playRound(player, computer) {
     // console.log(`${player} ${computer}`);
     if (player === computer) {
-        rp.textContent=`tie`;
-        console.log("tie");
+        rrs.textContent += `tie`;
+        return "tie";
     } else if (
-      (player === choices[0] && computer === choices[2]) ||
-      (player === choices[1] && computer === choices[0]) ||
-      (player === choices[2] && computer === choices[1])
+        (player === choices[0] && computer === choices[2]) ||
+        (player === choices[1] && computer === choices[0]) ||
+        (player === choices[2] && computer === choices[1])
     ) {
-        rp.textContent=`player`;
-      console.log("player");
+        rrs.textContent += `player`;
+        return "player";
     } else {
-        rp.textContent=`computer`;
-        console.log("computer");
+        rrs.textContent += `computer`;
+        return "computer";
     }
-  }
+}
 
- document.body.appendChild(resultDiv);
+document.body.appendChild(resultDiv);
 
+// to play a round match
+function playmatch(event) {
 
-  
+    let whoWon = playRound(
+        getPlayerChoice(btnChoice(event.target.id)),
+        getComputerChoice()
+    );
+    if (whoWon === "player") {
+        console.log('player');
+        pwin += 1;
+        console.log(pwin);
+    } else if (whoWon === "computer") {
+        console.log('computer');
+        cwin += 1;
+        console.log(cwin);
+    } else if (whoWon==="tie"){
+        console.log('round tie');
+    }
+    if (pwin ===5 && cwin===5) {
+        const mrs=document.getElementById('match-result')
+        mrs.textContent+='Tie'
+        console.log("match tie");
+    }
+    else if (pwin ===5) {
+        mrs.textContent+='Player Wins'
+        console.log("player wins");
+        pwin=0,cwin=0;
+    }
+    else if (cwin === 5) {
+        mrs.textContent='Computer Wins'
+        console.log("computer wins");
+        pwin=0,cwin=0;
+    }
+}
